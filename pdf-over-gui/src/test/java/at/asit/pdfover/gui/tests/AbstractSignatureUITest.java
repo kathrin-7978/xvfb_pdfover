da65c1ddbe3478788046d000df7610de8b274d10
kathrin.resek <kathrin.resek@a-sit.at>
2025-01-20 12:40:58 +0100

    minimal test setting

--- pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/AbstractSignatureUITest.java
+++ pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/AbstractSignatureUITest.java
@@ -1,114 +1,44 @@
 package at.asit.pdfover.gui.tests;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Properties;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swtbot.swt.finder.SWTBot;
-import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
-import org.eclipse.swtbot.swt.finder.waits.ICondition;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import at.asit.pdfover.commons.Constants;
-import at.asit.pdfover.commons.Messages;
-import at.asit.pdfover.commons.Profile;
-import at.asit.pdfover.gui.Main;
 import at.asit.pdfover.gui.workflow.StateMachine;
-import at.asit.pdfover.gui.workflow.config.ConfigurationManager;
-import lombok.NonNull;
+import at.asit.pdfover.gui.workflow.states.PositioningState;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public abstract class AbstractSignatureUITest {
+public abstract class AbstractSignatureUITest  {
 
     private static Thread uiThread;
     private static Shell shell;
-    private static StateMachine sm;
-    private SWTBotShell swtbs;
+    private static StateMachine sm = new StateMachine(new String[0]);
+    private final static CyclicBarrier swtBarrier = new CyclicBarrier(2);
     private SWTBot bot;
-
-    private static File inputFile = new File("src/test/resources/TestFile.pdf");
-    private static String outputDir = inputFile.getAbsoluteFile().getParent();
-    private String postFix = "_superSigned";
-    private Profile currentProfile;
-    private static final String UNIX_SEPARATOR = "/";
-
-    private static final Logger logger = LoggerFactory
-            .getLogger(AbstractSignatureUITest.class);
-
-    SignaturePositionTestProvider provider = new SignaturePositionTestProvider();
-    private static Path tmpDir;
-
-
-    private static List<Profile> profiles = new ArrayList<>();
-
-    protected String str(String k) { return Messages.getString(k); }
-
+	
     @BeforeAll
-    public static void prepareTestEnvironment() throws IOException {
-        deleteTempDir();
-        createTempDir();
-        setSignatureProfiles();
-    }
-
-    private static void deleteTempDir() throws IOException {
-        String root = inputFile.getAbsoluteFile().getParent();
-        File dir = new File(root);
-        for (File f : dir.listFiles()) {
-            if (f.getName().startsWith("output_")) {
-                FileUtils.deleteDirectory(f);
-            }
-        }
-    }
-
-    private static void createTempDir() throws IOException {
-        tmpDir = Files.createTempDirectory(Paths.get(inputFile.getAbsoluteFile().getParent()), "output_");
-        tmpDir.toFile().deleteOnExit();
-        outputDir = FilenameUtils.separatorsToSystem(tmpDir.toString());
-    }
-
-
-    @BeforeEach
-    public final void setupUITest() throws InterruptedException, BrokenBarrierException {
-        final CyclicBarrier swtBarrier = new CyclicBarrier(2);
-
+    public static synchronized void setupApp() {
         if (uiThread == null) {
-            uiThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                	currentProfile = getCurrentProfile();
-                	setConfig(currentProfile);					
-				
-                    sm = Main.setup(new String[]{inputFile.getAbsolutePath()});
-                    shell = sm.getMainShell();
+        uiThread = new Thread(new Runnable() {
 
+                @Override
+	            public void run() {
+                    shell = sm.getMainShell();
                     try {
-                        swtBarrier.await();
-                        sm.start();
+                        while (true) {
+                            // wait for the test setup
+                            swtBarrier.await();
+                            sm.start();
+                        }
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -117,129 +47,41 @@ public abstract class AbstractSignatureUITest {
             uiThread.setDaemon(true);
             uiThread.start();
         }
-        swtBarrier.await();
+    }
 
-        bot =  new SWTBot(shell);
-        swtbs = bot.activeShell();
-        swtbs.activate();
+    @BeforeEach
+    public final void setupSWTBot() throws InterruptedException, BrokenBarrierException {
+        // synchronize with the thread opening the shell
+        swtBarrier.await();		
+        bot = new SWTBot(shell);
     }
 
     @AfterEach
-    public void reset() throws InterruptedException {
-        deleteOutputFile();
-        closeShell();
-    }
-
     public void closeShell() throws InterruptedException {
+        // close the shell
         Display.getDefault().syncExec(new Runnable() {
             public void run() {
                 shell.close();
             }
         });
-        uiThread.join();
-        uiThread = null;
     }
 
+    protected void dragNDropPdfAndSign() {
+        SWTBotShell swtbs = bot.activeShell();
+        swtbs.activate();
 
-    protected void setCredentials() {
-        try {
-            ICondition widgetExists = new WidgetExitsCondition(str("mobileBKU.number"));
-            bot.waitUntil(widgetExists, 4000);
-            bot.textWithLabel(str("mobileBKU.number")).setText("TestUser-1902503362");
-            bot.textWithLabel(str("mobileBKU.password")).setText("123456789");
-            bot.button(str("common.Ok")).click();
+        sm.jumpToState(new PositioningState(sm));
+        String fileName = "./src/test/java/at/asit/pdfover/gui/tests/TestFile.pdf";
+        File documentPath = new File(fileName);
+        sm.status.document = documentPath;
 
-        }
-        catch (WidgetNotFoundException wnf) {
-            bot.button(str("common.Cancel")).click();
-            fail(wnf.getMessage());
-        }
+        bot.toggleButton("&Neue Seite anlegen").click();
+        bot.toggleButton("&Neue Seite rückgängig").click();
+        assertTrue(bot.button("&Signieren").isVisible());
+   
+  
+        
 
-        File output = new File(getPathOutputFile());
-        ICondition outputExists = new FileExistsCondition(output);
-        bot.waitUntil(outputExists, 6000);
-
-        if(!output.exists()) {
-            bot.button(str("common.Cancel")).click();
-        }
-        assertTrue(output.exists(), "Received signed PDF");
-    }
-
-    private void deleteOutputFile() {
-        if (getPathOutputFile() != null) {
-            File outputFile = new File(getPathOutputFile());
-            outputFile.delete();
-            assertTrue(!outputFile.exists());
-            logger.info("Deleted output file");
-        }
-    }
-
-    protected void testSignature(boolean negative, boolean captureRefImage) throws IOException {
-        String outputFile = getPathOutputFile();
-        assertNotNull(currentProfile);
-        assertNotNull(outputFile);
-        provider.checkSignaturePosition(currentProfile, negative, getPathOutputFile(), captureRefImage);
-    }
-
-    private String getPathOutputFile() {
-        String inputFileName = inputFile.getName();
-        String pathOutputFile = inputFileName
-                .substring(0, inputFileName.lastIndexOf('.'))
-                .concat(postFix)
-                .concat(".pdf");
-        pathOutputFile = FilenameUtils.separatorsToSystem(outputDir
-                .concat(UNIX_SEPARATOR)
-                .concat(pathOutputFile));
-        assertNotNull(pathOutputFile);
-        return pathOutputFile;
-    }
-
-    private static void setProperty(@NonNull Properties props, @NonNull String key, @NonNull String value) { props.setProperty(key, value); }
-
-    private void setConfig(Profile currentProfile) {
-        ConfigurationManager cm = new ConfigurationManager();
-        Point size = cm.getMainWindowSize();
-
-        Map<String,String> testParams = Map.ofEntries(
-                Map.entry(Constants.CFG_BKU, cm.getDefaultBKUPersistent().name()),
-                Map.entry(Constants.CFG_KEYSTORE_PASSSTORETYPE, "memory"),
-                Map.entry(Constants.CFG_LOCALE, cm.getInterfaceLocale().toString()),
-                Map.entry(Constants.CFG_LOGO_ONLY_SIZE, Double.toString(cm.getLogoOnlyTargetSize())),
-                Map.entry(Constants.CFG_MAINWINDOW_SIZE, size.x + "," + size.y),
-                Map.entry(Constants.CFG_OUTPUT_FOLDER, outputDir),
-                Map.entry(Constants.CFG_POSTFIX, postFix),
-                Map.entry(Constants.CFG_SIGNATURE_NOTE, currentProfile.getDefaultSignatureBlockNote(Locale.GERMANY)),
-                Map.entry(Constants.CFG_SIGNATURE_POSITION, "auto"),
-                Map.entry(Constants.SIGNATURE_PROFILE, currentProfile.toString()),
-                Map.entry(Constants.CFG_SIGNATURE_LOCALE, cm.getSignatureLocale().toString())
-                );
-
-        File pdfOverConfig = new File(Constants.CONFIG_DIRECTORY + File.separator + Constants.DEFAULT_CONFIG_FILENAME);
-        Properties props = new Properties();
-        testParams.forEach((k, v) -> setProperty(props, k, v));
-
-        try {
-            FileOutputStream outputstream = new FileOutputStream(pdfOverConfig, false);
-	        props.store(outputstream, "TEST Configuration file was generated!");
-        } catch (IOException e) {
-            logger.warn("Failed to create configuration file.");
-		}
-    }
-
-    public static void setSignatureProfiles() {
-        for (Profile p : Profile.values()) {
-            profiles.add(p);
-        }
-        assert(EnumSet.allOf(Profile.class).stream().allMatch(profiles::contains));
-    }
-
-    public Profile getCurrentProfile() {
-        currentProfile = profiles.get(0);
-        profiles.remove(0);
-        if (profiles.isEmpty()) {
-           	setSignatureProfiles();
-           }
-        return currentProfile;
     }
 
 }
--- pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/SignatureUITest.java
+++ pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/SignatureUITest.java
@@ -1,38 +1,13 @@
 package at.asit.pdfover.gui.tests;
 
-import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-import at.asit.pdfover.commons.Profile;
-
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SignatureUITest extends AbstractSignatureUITest{
 
-    @Order(1)
-    @ParameterizedTest
-    @EnumSource(Profile.class)
-    public void createRefFiles(Profile profile) throws IOException {
-        setCredentials();
-        testSignature(false, true);
-    }
+	@Test
+	public void testDragNDropPdfAndSign(){
+		dragNDropPdfAndSign();
+	}
 
-    @Order(2)
-    @ParameterizedTest
-    @EnumSource(Profile.class)
-    public void testSignaturAutoPosition(Profile profile) throws IOException {
-        setCredentials();
-        testSignature(false, false);
-    }
-
-    @Order(3)
-    @ParameterizedTest
-    @EnumSource(Profile.class)
-    public void testSignaturAutoPositionNegative(Profile profile) throws IOException {
-        setCredentials();
-        testSignature(true, false);
-    }
 }
+

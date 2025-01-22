230a68ab7b2ce8df6d75419bd9ebdb0e0b3ee1ee
kathrin.resek <kathrin.resek@a-sit.at>
2025-01-22 09:30:55 +0100

    test current setting with single profile

--- pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/SignatureUITest.java
+++ pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/SignatureUITest.java
@@ -1,13 +1,40 @@
 package at.asit.pdfover.gui.tests;
 
-import org.junit.jupiter.api.Test;
+import java.io.IOException;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import at.asit.pdfover.commons.Profile;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SignatureUITest extends AbstractSignatureUITest{
 
+	@Order(1)
 	@Test
-	public void testDragNDropPdfAndSign(){
-		dragNDropPdfAndSign();
+	public void createRefFiles() throws IOException {
+		setCredentials();
+		testSignature(false, true);
 	}
 
-}
+	@Order(2)
+	@Test
+	public void testSignaturAutoPosition() throws IOException {
+		setCredentials();
+		testSignature(false, false);
+	}
 
+	/*
+	@Order(3)
+	@ParameterizedTest
+	@EnumSource(Profile.class)
+	public void testSignaturAutoPositionNegative(Profile profile) throws IOException {
+		setCredentials();
+		testSignature(true, false);
+	}
+
+	 */
+}

7dab48291727ccc58ce1c1afa10e86a3c0c124eb
kathrin.resek <kathrin.resek@a-sit.at>
2025-01-29 12:28:18 +0100

    test func

--- pdf-over-gui/src/test/java/at/asit/pdfover/gui/utils/SimpleUITest.java
+++ pdf-over-gui/src/test/java/at/asit/pdfover/gui/utils/SimpleUITest.java
@@ -36,7 +36,7 @@ public class SimpleUITest {
 
     @Test
     public void testButtonClick() {
-        assertTrue(bot.button("Click Me").isVisible());
+        assertTrue(bot.button("Click M").isVisible());
     }
 
     @AfterEach

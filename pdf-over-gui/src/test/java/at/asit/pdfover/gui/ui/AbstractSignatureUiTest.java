ce9125a26e315fc67fa564d6f6e5ea733d12739b
kathrin.resek <kathrin.resek@a-sit.at>
2025-01-29 09:19:48 +0100

    run automated mac test

---
+++ pdf-over-gui/src/test/java/at/asit/pdfover/gui/ui/AbstractSignatureUiTest.java
@@ -0,0 +1,68 @@
+package at.asit.pdfover.gui.ui;
+
+import at.asit.pdfover.commons.Messages;
+import at.asit.pdfover.gui.Main;
+import at.asit.pdfover.gui.workflow.StateMachine;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.waits.ICondition;
+import org.junit.jupiter.api.BeforeEach;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AbstractSignatureUiTest {
+
+    private static Thread uiThread;
+    private static Shell shell;
+    private static StateMachine sm;
+    private SWTBotShell swtbs;
+    private SWTBot bot;
+
+    private static File inputFile = new File("src/test/resources/TestFile.pdf");
+    protected String str(String k) { return Messages.getString(k); }
+
+    @BeforeEach
+    public final void setupUITest() throws InterruptedException, BrokenBarrierException {
+        final CyclicBarrier swtBarrier = new CyclicBarrier(2);
+
+        if (uiThread == null) {
+            uiThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+
+                    sm = Main.setup(new String[]{inputFile.getAbsolutePath()});
+                    shell = sm.getMainShell();
+
+                    try {
+                        swtBarrier.await();
+                        sm.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            uiThread.setDaemon(true);
+            uiThread.start();
+        }
+        swtBarrier.await();
+
+        bot =  new SWTBot(shell);
+        swtbs = bot.activeShell();
+        swtbs.activate();
+    }
+
+
+    @Test
+    public void buttonClickTest() {
+        ICondition widgetExists = new WidgetExitsCondition(str("mobileBKU.numb3r"));
+        bot.waitUntil(widgetExists, 20000);
+    }
+}
---
+++ pdf-over-gui/src/test/java/at/asit/pdfover/gui/ui/WidgetExitsCondition.java
@@ -0,0 +1,23 @@
+package at.asit.pdfover.gui.ui;
+
+import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
+
+public class WidgetExitsCondition extends DefaultCondition {
+
+    private final String widgetName;
+
+    public WidgetExitsCondition(String widget) {
+        this.widgetName = widget;
+    }
+
+    @Override
+    public boolean test() {
+        return bot.textWithLabel(widgetName).isVisible();
+    }
+
+    @Override
+    public String getFailureMessage() {
+        return String.format("Could not find widget %s", widgetName);
+    }
+
+}

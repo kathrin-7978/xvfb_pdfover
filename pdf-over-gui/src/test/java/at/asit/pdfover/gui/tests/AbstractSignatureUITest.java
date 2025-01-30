aa68108e4611e5dc8f95b6dd62c22677b45f8fac
kathrin.resek <kathrin.resek@a-sit.at>
2025-01-30 10:00:53 +0100

    Revert "test func minimal test setting"
    This reverts commit 71a58a0aba05c1163a252f042fe0551607ccb7f1.

--- pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/AbstractSignatureUITest.java
+++ pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/AbstractSignatureUITest.java
@@ -25,7 +25,6 @@ import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,7 +52,7 @@ public abstract class AbstractSignatureUITest {
     private static File inputFile = new File("src/test/resources/TestFile.pdf");
     private static String outputDir = inputFile.getAbsoluteFile().getParent();
     private String postFix = "_superSigned";
-    private Profile currentProfile = Profile.SIGNATURBLOCK_SMALL;
+    private Profile currentProfile;
     private static final String UNIX_SEPARATOR = "/";
 
     private static final Logger logger = LoggerFactory
@@ -100,7 +99,7 @@ public abstract class AbstractSignatureUITest {
                 @Override
                 public void run() {
                     currentProfile = getCurrentProfile();
-                    //setConfig();
+                    setConfig();
                     //Display.getDefault().syncExec(() -> {
                     sm = Main.setup(new String[]{inputFile.getAbsolutePath()});
                     shell = sm.getMainShell();
@@ -143,24 +142,20 @@ public abstract class AbstractSignatureUITest {
     }
 
 
-    @Test
     protected void setCredentials() {
         try {
             ICondition widgetExists = new WidgetExitsCondition(str("mobileBKU.number"));
             bot.waitUntil(widgetExists, 20000);
 
-
-
-           // bot.textWithLabel(str("mobileBKU.number")).setText("TestUser-1902503362");
-          //  bot.textWithLabel(str("mobileBKU.password")).setText("123456789");
-          //  bot.button(str("common.Ok")).click();
+            bot.textWithLabel(str("mobileBKU.number")).setText("TestUser-1902503362");
+            bot.textWithLabel(str("mobileBKU.password")).setText("123456789");
+            bot.button(str("common.Ok")).click();
 
         }
         catch (WidgetNotFoundException wnf) {
             bot.button(str("common.Cancel")).click();
             fail(wnf.getMessage());
         }
-        /*
 
         File output = new File(getPathOutputFile());
         ICondition outputExists = new FileExistsCondition(output);
@@ -170,8 +165,6 @@ public abstract class AbstractSignatureUITest {
             bot.button(str("common.Cancel")).click();
         }
         assertTrue(output.exists(), "Received signed PDF");
-        */
-
     }
 
     private void deleteOutputFile() {
